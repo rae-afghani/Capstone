29ccc285e0ac0353422412517822df2b80a2dc21
rka22 <rka22@RAEAFGHANI>
2022-11-15 21:02:14 -0600

    Add project files.

---
+++ CapstoneV4/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using CapstoneV4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CapstoneV4.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
